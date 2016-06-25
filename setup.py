a0be856b4e024a232606fe65a61e1a871fd43449
Alice Bevan–McGregor <alice@gothcandy.com>
2016-06-25 17:28:18 -0400

    Added release information.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ elif sys.version_info > (3, 0) and sys.version_info < (3, 2):
 	raise SystemExit("CPython 3.3 or Pypy 3 (3.2) or later is required.")
 
 version = description = url = author = author_email = ""  # Silence linter warnings.
-exec(open(os.path.join("web", "core", "release.py")).read())  # Actually populate those values.
+exec(open(os.path.join("web", "session", "release.py")).read())  # Actually populate those values.
 
 
 class PyTest(TestCommand):
